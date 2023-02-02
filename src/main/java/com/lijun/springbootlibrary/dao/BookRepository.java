c4d9aae97d42e92bbb15853b3d8064a0af491ae2
lilianwang <wanglijun00123172@gmail.com>
2023-02-02 09:50:21 -0500

    Find by title, by category

--- src/main/java/com/lijun/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/lijun/springbootlibrary/dao/BookRepository.java
@@ -1,8 +1,12 @@
 package com.lijun.springbootlibrary.dao;
 
 import com.lijun.springbootlibrary.entity.Book;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
+  Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
+  Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
 }
