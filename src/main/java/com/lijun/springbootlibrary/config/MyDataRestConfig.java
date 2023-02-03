1b1219283d364ddb4acb0822caf1bfec477a4495
lilianwang <wanglijun00123172@gmail.com>
2023-02-03 13:48:08 -0500

    Find Reviews by BookId

--- src/main/java/com/lijun/springbootlibrary/config/MyDataRestConfig.java
+++ src/main/java/com/lijun/springbootlibrary/config/MyDataRestConfig.java
@@ -1,6 +1,7 @@
 package com.lijun.springbootlibrary.config;
 
 import com.lijun.springbootlibrary.entity.Book;
+import com.lijun.springbootlibrary.entity.Review;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
@@ -24,14 +25,17 @@ public class MyDataRestConfig implements RepositoryRestConfigurer {
     };
 
     config.exposeIdsFor(Book.class);
+    config.exposeIdsFor(Review.class);
+
     disableHttpMethods(Book.class, config, theUnsupportedActions);
+    disableHttpMethods(Review.class, config, theUnsupportedActions);
 
     // Configure CORS Mapping
     cors.addMapping(config.getBasePath() + "/**")
             .allowedOrigins(theAllowedOrigins);
   }
 
-  private void disableHttpMethods(Class<Book> theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
+  private void disableHttpMethods(Class theClass, RepositoryRestConfiguration config, HttpMethod[] theUnsupportedActions) {
     config.getExposureConfiguration()
             .forDomainType(theClass)
             .withItemExposure((metadata, httpMethods)->httpMethods.disable(theUnsupportedActions))
--- src/main/java/com/lijun/springbootlibrary/dao/ReviewRepository.java
+++ src/main/java/com/lijun/springbootlibrary/dao/ReviewRepository.java
@@ -1,7 +1,12 @@
 package com.lijun.springbootlibrary.dao;
 
 import com.lijun.springbootlibrary.entity.Review;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+  Page<Review> findByBookId(@RequestParam("book_id") Long bookId,
+                            Pageable pageable);
 }
