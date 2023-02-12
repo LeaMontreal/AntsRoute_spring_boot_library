84f34d7fa3434d01929c94148e17c3125f9439ff
lilianwang <wanglijun00123172@gmail.com>
2023-02-12 15:32:43 -0500

    myDebugForOkta

--- src/main/java/com/lijun/springbootlibrary/controller/ReviewController.java
+++ src/main/java/com/lijun/springbootlibrary/controller/ReviewController.java
@@ -3,12 +3,16 @@ package com.lijun.springbootlibrary.controller;
 import com.lijun.springbootlibrary.requestmodels.ReviewRequest;
 import com.lijun.springbootlibrary.service.ReviewService;
 import com.lijun.springbootlibrary.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
+  @Value("${myDebugForOkta}")
+  private String myDebugForOkta;
+
   private ReviewService reviewService;
 
   public ReviewController (ReviewService reviewService) {
@@ -18,7 +22,12 @@ public class ReviewController {
   @GetMapping("/secure/user/book")
   public Boolean reviewBookByUser(@RequestHeader(value="Authorization") String token,
                                   @RequestParam Long bookId) throws Exception {
-    String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+    String userEmail = "";
+    if (myDebugForOkta.equals("true")) {
+      userEmail = "watera@gmail.com";
+    } else {
+      userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+    }
 
     if (userEmail == null) {
       throw new Exception("User email is missing");
@@ -29,7 +38,12 @@ public class ReviewController {
   @PostMapping("/secure")
   public void postReview(@RequestHeader(value="Authorization") String token,
                          @RequestBody ReviewRequest reviewRequest) throws Exception {
-    String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+    String userEmail = "";
+    if (myDebugForOkta.equals("true")) {
+      userEmail = "watera@gmail.com";
+    } else {
+      userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+    }
     if (userEmail == null) {
       throw new Exception("User email is missing");
     }
